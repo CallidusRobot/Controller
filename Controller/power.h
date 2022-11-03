8e1765a0937018b1cf6b05976467c73243ddc1af
Benedikt Müssig <benedikt@gmx.info>
2022-11-03 10:30:17 +0100

    Created the static stub classes

---
+++ Controller/power.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+class CallidusPower {
+  public:
+  
+  private:
+  CallidusPower() {}
+};
---
+++ Controller/sensors.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+class CallidusSensors {
+  public:
+  
+  private:
+  CallidusSensors() {}
+};
